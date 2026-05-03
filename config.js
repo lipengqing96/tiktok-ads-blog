c16dfb37c917c682f63a4f0c1f67b62e39acaa5f
lipengqing96 <dired667788@gmail.com>
2026-05-03 22:38:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-/**https://click.quk.com/tracking?code=BKxDyyHZlK&sub_id=&sub_id2=&url=
+/*
  * 在此处填入您的 TikTok Ads 充值 / 邀请链接（广告联盟提供）。
  * 请勿在公开场合提交含密钥的配置；部署前确认链接有效。
  */
-window.TIKTOK_ADS_REFERRAL_URL = "https://ads.tiktok.com/";
+window.TIKTOK_ADS_REFERRAL_URL = "https://click.quk.com/tracking?code=BKxDyyHZlK&sub_id=&sub_id2=&url=";
